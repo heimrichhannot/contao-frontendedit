0c36b92df981b615303088330b2af11b0e84cd17
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-03-22 15:52:13 +0100

    fixed runonce

--- config/runonce.php
+++ config/runonce.php
@@ -16,7 +16,7 @@ class FrontendEditListRunOnce extends \Controller
 	{
 		$objDatabase = \Database::getInstance();
 
-		if ($objDatabase->fieldExists('tableFields', 'tl_module')) {
+		if (!$objDatabase->fieldExists('tableFields', 'tl_module')) {
 			return;
 		}
 

6229c7d0cf36200ddbafe49efb8bbbe043ed88d9
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-05-10 13:40:01 +0200

    fixed alerts

--- assets/js/jquery.frontendedit.js
+++ assets/js/jquery.frontendedit.js
@@ -4,30 +4,18 @@
 		init: function () {
 			this.initAlerts();
 		},
-		initAlertify: function(labelOK, labelCancel) {
-			alertify.set({
-				labels: {
-					ok     : labelOK,
-					cancel : labelCancel
-				}
-			});
-		},
 		initAlerts: function () {
-			if (typeof alertify === 'undefined')
-				return;
-
-			FRONTENDEDIT.initAlertify('Ja', 'Nein');
-
 			$('.formhybrid-list .delete, .frontendedit-list .delete').on('click', function(event) {
-				var $this = $(this);
+				if (typeof Bootstrapper !== 'undefined')
+				{
+					var $this = $(this);
 
-				event.preventDefault();
+					event.preventDefault();
 
-				alertify.confirm($this.data('message'), function (e) {
-					if (e) {
+					Bootstrapper.confirm($this.data('message'), function() {
 						window.location.href = $this.attr('href');
-					}
-				});
+					});
+				}
 			});
 		}
 	};
